d94b8303300908df3ccd90c4f0dc9a9420843ded
Ryan Case <rmcase@gmail.com>
2014-09-03 19:26:19 -0400

    Refactored the class structure to use Stones and Groups instead of Vertices.

--- go.py
+++ go.py
@@ -1,21 +1,33 @@
+from enum import Enum
+
+
 class BoardError(Exception):
     pass
 
 
-class GoVertex(object):
-    def __init__(self):
-        self.x_position = None
-        self.y_position = None
-        self.contents = None
+class StoneColor(Enum):
+    black = 0
+    white = 1
+
+
+class Stone(object):
+    def __init__(self, color):
+        self.color = color
+        self.group = None
 
     def __str__(self):
-        if self.contents is not None:
-            if self.contents == "white":
-                return "* "
+        if self.color is not None:
+            if self.color == StoneColor.white:
+                return "*"
             else:
-                return "o "
-        else:
-            return ". "
+                return "o"
+
+
+class Group(object):
+    def __init__(self, board):
+        self.board = board
+        self.members = []
+        self.size = len(self.members)
 
 
 class Goban(object):
@@ -28,12 +40,27 @@ class Goban(object):
             vals = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             self.base_values = list(vals[:size])
             self.size = size
-            self.board = [GoVertex() for _ in range(size * size)]
-            # Using the array approach:
-            # East Stone: index + 1
-            # West Stone: index - 1
-            # North Stone: index - size
-            # South Stone: index + size
+            self.board = [None for _ in range(size * size)]
+            self.groups = []
+
+    def get(self, index):
+        return self.board[index]
+
+    def __str__(self):
+        s = "  "
+        for i in range(self.size):
+            s += "{0} ".format(self.base_values[i])
+        for i in range(len(self.board)):
+            if i % self.size == 0:
+                s += "\n"
+                s += "{} ".format(self.base_values[i // self.size])
+            v = self.board[i]
+            if v is None:
+                s += ". "
+            else:
+                s += "{} ".format(v)
+        s += "\n"
+        return s
 
     def position_tuple_for_index(self, index):
         x = self.base_values[index % self.size]
@@ -45,17 +72,28 @@ class Goban(object):
         y = self.base_values.index(position[1])
         return y * self.size + x
 
-    def __str__(self):
-        s = ""
-        for i in range(len(self.board)):
-            if i % self.size == 0:
-                s += "\n"
-            s += str(self.board[i])
-        s += "\n"
-        return s
+    def place_stone(self, stone_color, position):
+        index = self.index_from_position_tuple(position)
+        stone = Stone(stone_color)
+        self.board[index] = stone
+
+    def north_index(self, index):
+        return index - self.size
+
+    def east_index(self, index):
+        return index + 1
+
+    def south_index(self, index):
+        return index + self.size
+
+    def west_index(self, index):
+        return index - 1
 
 
 def go_main():
     size = 9
     b = Goban(size)
+    b.place_stone(StoneColor.black, ('1', '1'))
+    b.place_stone(StoneColor.white, ('2', '2'))
+
     print(b)
